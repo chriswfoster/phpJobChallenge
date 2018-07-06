b831386d5bf56ce453235e4e9e44053b0a879fd0
Chris Foster <chriswfoster@gmail.com>
2018-07-06 14:49:16 -0500

    functions, strings, highorder stuff, etc

--- learnphp.php
+++ learnphp.php
@@ -17,17 +17,86 @@ echo $streetAddress . "</br>";
 echo $cityAddress . "</br>";
 
 
+////////////////////////////FUNCTIONS
+
+function addNumbers ($num1, $num2){ // params still use $.
+    return $num1 + $num2;
+}
+
+echo "3 + 4 = " . addNumbers(3, 4)
+
+
+
+
+//////////////////////////// STRINGS:
+/*
+$randString = "             Random String     ";
+$noSpaceString = "Random String";
+$partOfString= substr($noSpaceString, 0 ,6) . "</br>"; //same as javascripts substring
+echo $partOfString;
+
+echo strlen("$randString") . "<br/>";
+echo strlen(ltrim("$randString")) . "<br />"; // trims left side of string using ltrim
+echo strlen(trim($randString)) . "<br />"; //trims all of string, when no l or r is defined
+
+echo "The randomString is $randString </br>";
+printf ("The randomString is %s </br>", $randString); // takes 2nd parameter and passes it in. Use %s to tell it to grab the string.
+echo strtoupper($randString) . '</br>'; // uppercase the entire string
+echo strtolower($randString) . '</br>';// lowercase the string
+echo ucfirst($randString) . '</br>'; // upper case first letter of string words
+
+$arrayForSTring = explode(' ', $randString, 2); //breaks string up, and puts it into an array.
+$stringToArray = implode(' ', $arrayForSTring ); //breaks array down into a string
+
+//$decimalNum = 2.3456;
+//printf("decimal num = %.2f </br>"); //using printf with conversion code to shrink decimal down.
+
+//other conversion codes:
+//    b: integer to binary
+//    c: integer to character
+//    d: integer to decimal
+//    f: double to float
+//    o: integer to octal
+//    s: string to string
+
+$man = "Man";
+$manhole = "Manhole";
+//echo strcmp($man, $manhole); // compares 2 strings. returns a number. 
+                                //returns -4 b/c 1st param is smaller than 2nd param.
+//echo strcasecmp($man, $manhole); // compares strings, but ignores case sensitivity.
+
+echo "Prints out part of string when it finds the world in strstr: " . strstr($manhole, "hole");
+// stristr() is case insensitive. 
+echo "</br>";
+echo "Index of the word hole: " . strpos($manhole, "hole");
+echo "</br>";
+echo "Replace part of string: " . str_replace("hole", "cover", $manhole);
+*/
+
+
+//////////////////////////// Common array functions:
+/*
+$yourArray = array('tom', 'larry', 'chuck', 'steve');
+//sort($yourArray); // sorts in ascending alphabetical order
+//sort($yourArray, SORT_NUMERIC); // will sort numerically 
+//sort($yourArray, SORT_STRING); // sorts strings
+//asort($yourArray); // sorts array with keys
+//ksort($yourArray); //sorts by keys
+rsort($yourArray); //add the letter 'r' to the beginning of any sort function, and it reverses it.
+foreach($yourArray as $person){
+    echo $person;
+}
+*/
 
 //////////////////////////// MultiDimensional Array
-
+/*
 $customers = array(array('Derek', '123 Main', '15212'), array('Sally', '125 Main', '15212'), array('Tom', '124 Main', '15212'));
-
 foreach($customers as $row){
     foreach ($row as $columns){
         echo $columns;
     }
 }
-
+*/
 
 //////////////////////////// ARRAYS
 /*
