41275f392e3ae9e8a0fc5b649f75f6372de1bd21
Chris Foster <chriswfoster@gmail.com>
2018-07-06 18:57:44 -0500

    Troubleshooting jquery

--- customerSearch.php
+++ customerSearch.php
@@ -1,20 +1,26 @@
 <html>
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <body>
 
 <p>Search for users:</p>
     <form method="post">
         <table border="0">
             <tr>
-                <td>Clients Name:</td>
-                <td align="center">
-                    <input placeholder="Type Client Name" type="text" name="user" size="30" />
+                <td>Search by:</td>
+                <td>
+                    <select name="selected">
+                        <option value="name">Name</option>
+                        <option value="id">ID</option>
+                    </select>
                 </td>
             </tr>
+
             <tr>
-                <td>Password</td>
+                <td id="clientsname">Clients Name:</td>
+                <td id="clientsid">Client ID:</td>
+                
                 <td align="center">
-                    <input type="text" name="userpassword" size="30" />
+                    <input placeholder="Type Client Name" type="text" name="user" size="30" />
                 </td>
             </tr>
             <tr>
@@ -22,8 +28,17 @@
                     <input type="submit" value="Submit" />
                 </td>
             </tr>
+           
         </table>
     </form>
+
+<script> 
+$("#clientsid").hide()
+$("button").click(function(){
+    $("#clientsname").toggle()
+    $("#clientsid").toggle()
+});
+</script>
     <?php
 
 
