3da29052e7e98e14eeaaa57349d10b1c9b2b32c9
Chris Foster <chriswfoster@gmail.com>
2018-07-06 15:37:20 -0500

    added sql queries

---
+++ SQL queries/createUser.sql
@@ -0,0 +1,6 @@
+insert into monkedia_users(username, password, email)
+values(
+'chris',
+'test',
+'chriswfoster@gmail.com'
+)
---
+++ SQL queries/createUserTable.sql
@@ -0,0 +1,6 @@
+create table monkedia_users(
+id int AUTO_INCREMENT primary key,
+username varchar(100),
+password varchar(255),
+email varchar(255)
+)
