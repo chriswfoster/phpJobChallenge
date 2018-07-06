852b615142afb5ebb468b29bc11d914c61a022c2
Chris Foster <chriswfoster@gmail.com>
2018-07-06 14:15:08 -0500

    Arrays, foreach, for loops, whiles, etc

--- learnphp.php
+++ learnphp.php
@@ -5,6 +5,8 @@
 
 <body>
 <?php
+// include 'confidential.php'; // how to import 1 php file data into another
+
 
 $usersName = $_POST['username']; // this will get me the username data from input.
 $streetAddress = $_POST['streetaddress'];
@@ -15,25 +17,86 @@ echo $streetAddress . "</br>";
 echo $cityAddress . "</br>";
 
 
-// $str = <<<EOD
-// The customers name is
-// $usersName and they
-// live at $streetAddress
-// in $cityAddress. </br>
-// EOD;
 
-echo $str;
+//////////////////////////// MultiDimensional Array
 
-$numOfOranges = 4;
-$numOfBananas = 36;
+$customers = array(array('Derek', '123 Main', '15212'), array('Sally', '125 Main', '15212'), array('Tom', '124 Main', '15212'));
 
-echo $testData;
+foreach($customers as $row){
+    foreach ($row as $columns){
+        echo $columns;
+    }
+}
 
-if ($numOfOranges > 25 && $numOfBananas > 30){
-    echo '25% discount given';
-} elseif ($numOfOranges > 30 || $numOfBananas > 35){
-    echo '15% discount given';
-    } else echo 'no discount given';
+
+//////////////////////////// ARRAYS
+/*
+$bestFriends = array('Joy', 'Willow', 'Ivy');
+$bestFriends[3] = 'Mike';
+for ($num =0; $num < count($bestFriends); $num++){
+    echo $bestFriends[$num] . ', ' . "<br />";
+} 
+// can also do a foreach to map through it. This is just for arrays.
+foreach($bestFriends as $friend){
+    echo $friend . ', ';
+}
+echo "<br />";
+// key valued pairs for arrays:
+$customer = array('Name' => $usersName, 'Street' => $streetAddress, 'City' => $cityAddress);
+foreach($customer as $key => $value){
+    echo $key . ' : ' . $value . "<br />";
+}
+$bestFriends = $bestFriends + $customer;
+// echo $bestFriends;
+foreach($bestFriends as $person){
+    echo $person . ', ';
+}
+*/
+
+//////////////////////////// FOR LOOP
+/*
+for ($num = 1; $num <= 20; $num++){
+    echo $num;
+
+    if($num != 20){
+        echo ', ';
+    } else {
+        break; // breaks the for loop, could use exit() here as well, but that will stop your PHP script all together.
+    }
+}
+*/
+
+//////////////////////////// WHILE LOOP
+/*
+$num = 0;
+ while ($num < 20){
+echo ++$num . ', '; // if i put ++ after $num, it'd return 0, then add the numbers, I won't get num till the next go around.
+ }
+*/
+////////////////////////////SWITCH STATEMENT
+// switch ($usersName){
+// case "Chris" :
+//     echo "Hello Chris";
+//     break;
+// case "Sally" :
+// echo "Hello Sally";
+// break;
+// default :
+// echo "Hello unknown person";
+// }
+//////
+
+
+/////////////////////////
+// $numOfOranges = 4;
+// $numOfBananas = 36;
+// if ($numOfOranges > 25 && $numOfBananas > 30){
+//     echo '25% discount given';
+// } elseif ($numOfOranges > 30 || $numOfBananas > 35){
+//     echo '15% discount given';
+//     } else echo 'no discount given';
+//////////////////////////
+
 
 // if user buys more than 25 oranges, and more than 30 bananas.
 
@@ -61,6 +124,19 @@ if ($numOfOranges > 25 && $numOfBananas > 30){
 // you can define constants -  that can't be changed, utilzing the 'define' keyword:
 // define('PI', 3.1415926); // you want to use uppercase with your constants.
 // echo "The value of PI is " . PI;
+
+////////////////////////// heredoc string
+/* 
+$str = <<<EOD
+ The customers name is
+ $usersName and they
+ live at $streetAddress
+ in $cityAddress. </br>
+EOD; // <- needs to have no space in front of it.
+ echo $str;
+ */
+//////////////////////////
+
 
 /*
 Data types are:
