223b85ae71b14dccc684d38c75a452e3219b8d13
Chris Foster <chriswfoster@gmail.com>
2018-07-06 19:26:30 -0500

    Styling and conditional rednering complete

--- customerSearch.php
+++ customerSearch.php
@@ -1,14 +1,15 @@
 <html>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="/CSS/customerSearch.css">
+<body class="cSBody">
 
 <p>Search for users:</p>
     <form method="post">
         <table border="0">
             <tr>
-                <td>Search by:</td>
+                <td class="tableSizing">Search by:</td>
                 <td>
-                    <select name="selected">
+                    <select id="clientSelect" name="selected">
                         <option value="name">Name</option>
                         <option value="id">ID</option>
                     </select>
@@ -16,13 +17,29 @@
             </tr>
 
             <tr>
-                <td id="clientsname">Clients Name:</td>
-                <td id="clientsid">Client ID:</td>
+                <td id="clientsname" class="tableSizing">Clients Name:</td>
+                <td id="clientsid" class="tableSizing">Client ID:</td>
                 
                 <td align="center">
-                    <input placeholder="Type Client Name" type="text" name="user" size="30" />
+                    <input id="clientSearchBox" placeholder="Type Client Name Here" type="text" name="user" size="30" />
                 </td>
             </tr>
+            <script> 
+// 
+// $("button").click(function(){
+//     $("#clientsname").toggle()
+//     $("#clientsid").toggle()
+// });
+$("#clientsid").hide()
+$("#clientSelect").change(function(e) {
+  e.target.value === "name" ? ($("#clientsid").hide() 
+  && $("#clientsname").show()
+  && $("#clientSearchBox").attr("placeholder", "Type Client Name Here")) 
+  : ($("#clientsid").show() 
+  && $("#clientsname").hide()
+  && $("#clientSearchBox").attr("placeholder", "Type Client ID Here"))
+});
+</script>
             <tr>
                 <td colspan="2" align="center">
                     <input type="submit" value="Submit" />
@@ -32,13 +49,7 @@
         </table>
     </form>
 
-<script> 
-$("#clientsid").hide()
-$("button").click(function(){
-    $("#clientsname").toggle()
-    $("#clientsid").toggle()
-});
-</script>
+
     <?php
 
 
